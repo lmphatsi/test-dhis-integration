70b9f9bfef09ea1ecbf84138817bfb1130f47e9d
lmphatsi <la.mphatsi@gmail.com>
2022-06-14 10:16:37 +0200

    Scheduling trail

--- src/main/java/com/possible/dhis2int/Application.java
+++ src/main/java/com/possible/dhis2int/Application.java
@@ -3,9 +3,11 @@ package com.possible.dhis2int;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan("com.possible.dhis2int")
+@EnableScheduling
 public class Application {
 	public static void main(String[] args) throws ClassNotFoundException {
 		Class.forName("com.mysql.jdbc.Driver");
--- src/main/java/com/possible/dhis2int/scheduler/DHIS2Scheduler.java
+++ src/main/java/com/possible/dhis2int/scheduler/DHIS2Scheduler.java
@@ -1,2 +1,25 @@
-package com.possible.dhis2int.scheduler;public class DHIS2Scheduler {
+package com.possible.dhis2int.scheduler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.annotation.Schedules;
+
+import java.util.Date;
+
+public class DHIS2Scheduler {
+
+    Logger logger = LoggerFactory.getLogger(DHIS2Scheduler.class);
+
+    private void submitToDHIS(String programmeName, Integer year, Integer month){
+
+    }
+
+    @Scheduled(fixedDelay = 5000)
+    public void processSchedules(){
+        //read from table
+        //for each row, is this report due
+        //if due => call submitToDHIS(x,y,z)
+        logger.info("Scheduler ran at :"+new Date().toString());
+    }
 }
